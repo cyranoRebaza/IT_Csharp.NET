941abdf22623bbd5a09aac88243933f89bd118df
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2024-11-14 11:50:55 -0300

    FEAT(.cs): se agrego concepto bucle infinito con while

---
+++ Csharp.NET/modulo2-controlFlujo/T06_while/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T06_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             ESTRUCTURA WHILE (mientras se cumpla la condicion ejecutara la sentencias asociadas)
+                SINTAXIS:
+                while(condicion)
+                {
+                    sentencia;
+                }
+
+             */
+            
+            
+            // mostrar el numero del 1 al 5            
+            int n = 1;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
+
+            Console.WriteLine("*************************");
+
+            // usando estructura while
+            int n2 = 1;
+
+            while (n2 <= 5)
+            {
+                Console.WriteLine(n2);
+                n2++;
+            }
+
+        }
+    }
+}
