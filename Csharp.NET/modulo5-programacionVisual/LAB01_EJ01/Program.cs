d4fc42f60c03955f410cda63296caf48e2b93e42
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2024-11-28 01:16:36 -0300

    FEAT(.cs): modulo5, laboratorio1

---
+++ Csharp.NET/modulo5-programacionVisual/LAB01_EJ01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB01_EJ01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaro la variable producto de tipo Producto
+            Producto producto;
+
+            // cargo los campos
+            Console.WriteLine("Ingrese el codigo del producto: ");
+            producto.codigo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el nombre del producto: ");
+            producto.nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingree el precio del producto: ");
+            producto.precio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la categoria del producto: ");
+            producto.categoria = Console.ReadLine();
+
+            // muestro los datos del producto
+            Console.WriteLine("Datos del Producto\n");
+            Console.WriteLine($"Codigo: {producto.codigo}");
+            Console.WriteLine($"Nombre: {producto.nombre}");
+            Console.WriteLine($"Precio: {producto.precio}");
+            Console.WriteLine($"Categoria: {producto.categoria}");
+
+        }
+
+
+
+    }
+
+    public struct Producto
+    {
+        public int codigo;
+        public string nombre;
+        public double precio;
+        public string categoria;
+    }
+}
+
+
+/*
+ 
+Crear una estructura para almacenar los datos
+pertenecientes a los productos de una tienda.
+Los datos a registrar para un producto son:
+    - Código.
+    - Nombre.
+    - Precio.
+    - Categoría.
+
+Codificar una aplicación de consola que permita
+cargar todos los datos de un producto y
+mostrarlos por pantalla.
+ 
+ 
+ */
---
+++ Csharp.NET/modulo5-programacionVisual/LAB01_EJ02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB01_EJ02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto[] productos = new Producto[3];
+
+            for (int i = 0; i < productos.Length; i++)
+            {
+                // cargo los campos
+                Console.WriteLine("Ingrese el codigo del producto: ");
+                productos[i].codigo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el nombre del producto: ");
+                productos[i].nombre = Console.ReadLine();
+
+                Console.WriteLine("Ingree el precio del producto: ");
+                productos[i].precio = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingrese la categoria del producto: ");
+                productos[i].categoria = Console.ReadLine();
+                Console.Clear();
+            }
+
+            Console.WriteLine("Datos de los productos: ");
+            Console.WriteLine("------------------------");
+
+            for (int i = 0; i < productos.Length; i++)
+            {
+                // muestro los datos del producto
+                Console.WriteLine($"Codigo: {productos[i].codigo}");
+                Console.WriteLine($"Nombre: {productos[i].nombre}");
+                Console.WriteLine($"Precio: {productos[i].precio}");
+                Console.WriteLine($"Categoria: {productos[i].categoria}");
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public struct Producto
+    {
+        public int codigo;
+        public string nombre;
+        public double precio;
+        public string categoria;
+    }
+}
