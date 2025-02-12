7ef7d4751e4de7d6a888535598296d7563664852
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2025-02-12 11:45:38 -0300

    FEAT(.cs): modulo6, ingreso y egreso de datos

---
+++ Csharp.NET/modulo6-windowsForms/WF02_interfazGrafica/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WF02_interfazGrafica
+{
+    partial class FormHolaMundo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lblIngreso = new System.Windows.Forms.Label();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.lblMostrar = new System.Windows.Forms.Label();
+            this.txtIngreso = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(163, 140);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(115, 33);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnMostrar_MouseClick);
+            // 
+            // lblIngreso
+            // 
+            this.lblIngreso.AutoSize = true;
+            this.lblIngreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngreso.Location = new System.Drawing.Point(58, 55);
+            this.lblIngreso.Name = "lblIngreso";
+            this.lblIngreso.Size = new System.Drawing.Size(77, 20);
+            this.lblIngreso.TabIndex = 1;
+            this.lblIngreso.Text = "Ingreso:";
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalida.Location = new System.Drawing.Point(58, 228);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(67, 20);
+            this.lblSalida.TabIndex = 2;
+            this.lblSalida.Text = "Salida:";
+            // 
+            // lblMostrar
+            // 
+            this.lblMostrar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostrar.Location = new System.Drawing.Point(163, 228);
+            this.lblMostrar.Name = "lblMostrar";
+            this.lblMostrar.Size = new System.Drawing.Size(285, 40);
+            this.lblMostrar.TabIndex = 3;
+            // 
+            // txtIngreso
+            // 
+            this.txtIngreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIngreso.Location = new System.Drawing.Point(163, 52);
+            this.txtIngreso.Name = "txtIngreso";
+            this.txtIngreso.Size = new System.Drawing.Size(285, 27);
+            this.txtIngreso.TabIndex = 4;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(333, 357);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(115, 33);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnSalir_MouseClick);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(311, 140);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(115, 33);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnLimpiar_MouseClick);
+            // 
+            // FormHolaMundo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 418);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.txtIngreso);
+            this.Controls.Add(this.lblMostrar);
+            this.Controls.Add(this.lblSalida);
+            this.Controls.Add(this.lblIngreso);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "FormHolaMundo";
+            this.Text = "HOLA MUNDO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label lblIngreso;
+        private System.Windows.Forms.Label lblSalida;
+        private System.Windows.Forms.Label lblMostrar;
+        private System.Windows.Forms.TextBox txtIngreso;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Csharp.NET/modulo6-windowsForms/WF02_interfazGrafica/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF02_interfazGrafica
+{
+    public partial class FormHolaMundo : Form
+    {
+        public FormHolaMundo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_MouseClick(object sender, MouseEventArgs e)
+        {
+            string txt;
+
+            txt = txtIngreso.Text;
+
+            lblMostrar.Text = txt;
+        }
+
+        private void btnSalir_MouseClick(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpiar_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtIngreso.Text = "";
+            lblMostrar.Text = "";
+
+            txtIngreso.Focus();
+        }
+    }
+}
